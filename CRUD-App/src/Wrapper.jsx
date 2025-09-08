1d5723528353698fc70eec1e4691f8a11c6f763f
niraj-verma07 <vermaniraj9607@gmail.com>
2025-09-08 11:14:43 +0530

    Wrapper HOC

---
+++ CRUD-App/src/Wrapper.jsx
@@ -0,0 +1,6 @@
+const Wrapper = (props) => {
+  console.log(props);
+  return props.children;
+};
+
+export default Wrapper;
--- CRUD-App/src/main.jsx
+++ CRUD-App/src/main.jsx
@@ -2,11 +2,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ToastContainer } from "react-toastify";
+import Wrapper from "./Wrapper.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <>
+  <Wrapper>
+    {/* Higher Order Components */}
     <App />
     <ToastContainer position="top-center" />
     {/* React Toastify configuration for using it */}
-  </>
+  </Wrapper>
 );
