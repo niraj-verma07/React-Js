d1c99078d854d772d91f71b27d94d559a2f0f528
niraj-verma07 <vermaniraj9607@gmail.com>
2025-10-25 22:38:07 +0530

    Prev Next Button done

--- Gallery-App/src/App.jsx
+++ Gallery-App/src/App.jsx
@@ -3,22 +3,26 @@ import { useEffect, useState } from "react";
 
 const App = () => {
   const [userData, setUserData] = useState([]);
-  const [index, Index] = useState(1);
+  const [index, setIndex] = useState(1);
 
   const getData = async () => {
     let response = await axios.get(
-      "https://picsum.photos/v2/list?page=3&limit=30"
+      `https://picsum.photos/v2/list?page=${index}&limit=10`
     );
     // console.log(response.data[1].download_url);
 
     setUserData(response.data);
   };
 
-  useEffect(function () {
-    getData();
-  }, []);
+  useEffect(
+    function () {
+      getData();
+    },
+    [index]
+    // index ke value change pe useeffect chlega
+  );
 
-  let printUserData = <h3 className="text-gray-500">"No User Available"</h3>;
+  let printUserData = <h3 className="text-gray-500 text-2xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">"Loading..."</h3>;
 
   if (userData.length > 0) {
     printUserData = userData.map((elem, idx) => {
@@ -47,12 +51,15 @@ const App = () => {
       >
         Get Data
       </button> */}
-
-      <div className="flex flex-wrap gap-4 p-2">{printUserData}</div>
+  
+      <div className="flex h-[92%] flex-wrap gap-4 p-2">{printUserData}</div>
       <div className="flex justify-center items-center p-4 gap-6">
         <button
           onClick={() => {
-            console.log("Prev");
+            if (index > 1) {
+              setIndex(index - 1);
+              setUserData([]);
+            }
           }}
           className="bg-amber-400 text-black px-4 py-2 rounded font-semibold text-sm cursor-pointer active:scale-95"
         >
@@ -60,7 +67,8 @@ const App = () => {
         </button>
         <button
           onClick={() => {
-            console.log("Next");
+            setIndex(index + 1);
+            setUserData([]);
           }}
           className="bg-amber-400 text-black px-4 py-2 rounded font-semibold text-sm cursor-pointer active:scale-95"
         >
