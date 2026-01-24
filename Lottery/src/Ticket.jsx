523a8b5998ec2d7b88e948d1949b7b015c195bb3
Niraj Kumar Verma <vermaniraj9607@gmail.com>
2026-01-24 07:43:16 +0530

    Update Ticket.jsx

--- Lottery/src/Ticket.jsx
+++ Lottery/src/Ticket.jsx
@@ -7,8 +7,6 @@ export default function Ticket({ticket}){
             <p>Ticket</p>
             {ticket.map((num, idx)=> (
                 <TicketNum num={num} key={idx}/>
-
-
             ))}
         </div>
     );
